a60973ce5e0b9bbddfb8d74a693cd85ab37c6cfe
Tomáš Borkovec <tomasborki@gmail.com>
2021-11-08 12:05:09 +0100

    Edit login page

---
+++ www/js/main.js
@@ -0,0 +1,16 @@
+
+
+
+function passwordView() {
+    $("#password").prop('type', 'text');
+}
+
+function passwordNo() {
+    $("#password").prop('type', 'password');
+}
+
+function changeBackgroundColor(){
+
+        $(".left-div").toggleClass("orange-gradient");
+
+}
